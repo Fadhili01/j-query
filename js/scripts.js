029ea844268ebe702b5bbd640ea86c90921818f4
Fadhili <fadhoturi01@gmail.com>
2020-04-02 00:14:02 +0300

    Correct indentation of the code

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 jQuery('h1').click(function(){
-    alert("This is a heading tag")
+  alert("This is a heading tag")
+});
+
+jQuery('p').click(function(){
+  alert("This is a paragraph")
   });
 
-  jQuery('p').click(function(){
-    alert("This is a paragraph")
-  });
-
-  jQuery('img').click(function(){
-    alert("This is an image")
-  });
+jQuery('img').click(function(){
+  alert("This is an image")
+});
